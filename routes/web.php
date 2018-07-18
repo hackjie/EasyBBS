ee5a295fca42f85053acf859576f42babc283e87
hackjie <codelijie@gmail.com>
2018-07-18 07:50:42 +0000

    User profile page

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,5 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
