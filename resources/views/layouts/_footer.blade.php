02da47f863f99b81f5e38198feb86bea959509a5
hackjie <codelijie@gmail.com>
2018-07-18 06:46:08 +0000

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="http://leoli.win" target="_blank">Leoli</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:name@email.com">联系我们</a></p>
+    </div>
+</footer>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <!-- route_class()此方法会将当前请求的路由名称转换为 CSS 类名称，作用是允许我们针对某个页面做页面样式定制 -->
+    <div id="app" class="{{ route_class() }}-page">
+
+        @include('layouts._header')
+
+        <div class="container">
+
+            @yield('content')
+
+        </div>
+
+        @include('layouts._footer')
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
