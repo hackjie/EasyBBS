8b9aa3319679764d67f01edd1b12a200c7528da8
hackjie <codelijie@gmail.com>
2018-07-19 03:25:47 +0000

    List topics created by user

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -43,7 +43,14 @@
 
                         <ul class="dropdown-menu" role="menu">
                             <li>
+                                <a href="{{ route('users.show', Auth::id()) }}">
+                                    <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                                    个人中心
+                                </a>
+                            </li>
+                            <li>
                                 <a href="{{ route('users.edit', Auth::id()) }}">
+                                    <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
                                     编辑资料
                                 </a>
                             </li>
@@ -51,6 +58,7 @@
                                 <a href="{{ route('logout') }}"
                                     onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
+                                    <span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>
                                     退出登录
                                 </a>
 
