3ca206f13df6bc876fdbd8cdb70c97be0280c0c2
hackjie <codelijie@gmail.com>
2018-07-19 06:10:10 +0000

    User can upload topic image

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,7 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
